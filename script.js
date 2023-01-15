a01ac19497c046da3e9d8eb9783f866b9e579e44
praduman2714 <pradu14jsr@gmail.com>
2023-01-15 20:15:13 +0530

    commited

---
+++ script.js
@@ -0,0 +1,54 @@
+var countInterval;
+
+
+function startCounter(){
+    var number = parseInt(document.getElementById("number").value);
+
+    if (isNaN(number)) {
+        alert("Please enter a number");
+        clearInterval(countInterval);   // This is important for the condition when a counter is running and you entered a wrong input for a new counter
+        return;
+    }
+    
+    if (number < 1 || number > 9) {
+        alert("Range out of bounds");
+        clearInterval(countInterval);
+        return;
+    }
+
+    var currentNo = document.querySelector(".counter .current");
+    var nextNo = document.querySelector(".counter .next");
+    var c = 0;
+
+    resetNumbers(currentNo, nextNo);
+    
+    // Clears the previous interval that was running
+    clearInterval(countInterval);
+
+    countInterval = setInterval(function () {
+        if (c === number) {
+            clearInterval(countInterval);
+            alert("Counter has stopped");
+            return;
+        }
+        increaseCount(currentNo, nextNo);
+        c++;
+    }, 1000);
+}
+
+
+function resetNumbers(curr, next){
+    curr.innerText = 0;
+    next.innerText = 1;
+}
+
+function increaseCount(currentNo, nextNo){
+
+    nextNo.classList.add("animate");
+    setTimeout(function(){
+        currentNo.innerText = nextNo.innerText;
+        nextNo.classList.remove("animate");
+        nextNo.innerText = parseInt(nextNo.innerText)+1;
+    }, 500);
+}
+
